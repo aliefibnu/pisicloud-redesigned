4da2df557737ee1888ead97ab93ac6f1750d59d9
ahmadah karima <ahmadahkarima@gmail.com>
2026-09-02 14:12:58 +0700

    anjay

--- src/app/components/landing/why-chose-us/why-chose-us.ts
+++ src/app/components/landing/why-chose-us/why-chose-us.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-why-choose-us',
-  templateUrl: './why-choose-us.component.html',
-  styleUrls: ['./why-choose-us.component.css']
+  templateUrl: './why-chose-us.html',
 })
 export class WhyChooseUsComponent {
   // Komponen ini bertindak sebagai presenter statis untuk struktur Bento Grid
