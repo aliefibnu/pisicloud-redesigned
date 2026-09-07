a74dcecce748fb64cedb3558a80ede045f35f995
Gavinnaufal <gavinnaufal26@gmail.com>
2026-09-07 14:57:54 +0700

    Add multilingual support for "About Us" and "Why Choose Us" sections with translations and dynamic content

--- src/app/components/universal/footer/footer.spec.ts
+++ src/app/components/universal/footer/footer.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideTranslateService } from '@ngx-translate/core';
 import { Footer } from './footer';
 
 describe('Footer', () => {
@@ -9,6 +9,7 @@ describe('Footer', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Footer],
+      providers: [provideTranslateService()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Footer);
