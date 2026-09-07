313e7086b8b36362e2d948877d0208b2ba5206a9
Gavinnaufal <gavinnaufal26@gmail.com>
2026-09-07 14:57:54 +0700

    Add multilingual support for "About Us" and "Why Choose Us" sections with translations and dynamic content

--- src/app/components/universal/footer/footer.ts
+++ src/app/components/universal/footer/footer.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'universal-footer, app-footer',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './footer.html',
 })
 export class Footer {
