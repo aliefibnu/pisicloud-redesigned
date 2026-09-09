f9b97735e2e8a401073657f5a82e78099b1b49eb
richardmarove <sorrowprivategt@gmail.com>
2026-09-09 10:56:18 +0700

    3d image change

--- src/app/components/our-customers/hero/hero.ts
+++ src/app/components/our-customers/hero/hero.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { bootstrapShieldCheck } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'customers-hero',
-  imports: [NgOptimizedImage, TranslatePipe, NgIcon],
-  viewProviders: [
-    provideIcons({
-      bootstrapShieldCheck,
-    }),
-  ],
+  imports: [NgOptimizedImage, TranslatePipe],
   templateUrl: './hero.html',
   styles: `
     :host {
