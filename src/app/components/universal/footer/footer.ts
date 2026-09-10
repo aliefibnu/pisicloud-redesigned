d0f70a10c3a287247b881a44e73477c103ab53ec
richardmarove <sorrowprivategt@gmail.com>
2026-09-10 16:14:58 +0700

    optimized customize-hero.webp and converted inforsys.png to inforsys.webp

--- src/app/components/universal/footer/footer.ts
+++ src/app/components/universal/footer/footer.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'universal-footer, app-footer',
-  imports: [CommonModule, RouterLink, TranslatePipe],
+  imports: [CommonModule, RouterLink, TranslatePipe, NgOptimizedImage],
   templateUrl: './footer.html',
 })
 export class Footer {
